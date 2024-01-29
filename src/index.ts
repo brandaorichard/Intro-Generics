d5191b4c19d176851c62d8c845148579424ca5fe
brandaorichard <rih.brandao@gmail.com>
2024-01-29 17:27:50 -0300

    type assertion: initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,36 @@
-function addProducts<T>(products: T[], newProduct: T): T[] {
-  return [...products, newProduct];
-}
+// function addProducts<T>(products: T[], newProduct: T): T[] {
+//   return [...products, newProduct];
+// }
 
-type Bread = {
-  name: string,
-  ingredients: string[],
-  gluten: boolean,
-};
+// type Bread = {
+//   name: string,
+//   ingredients: string[],
+//   gluten: boolean,
+// };
 
-type Flour = {
-  brand: string,
-  description: string,
-  gluten: boolean,
-};
+// type Flour = {
+//   brand: string,
+//   description: string,
+//   gluten: boolean,
+// };
 
-const breads: Bread[] = [];
-const flours: Flour[] = [];
+// const breads: Bread[] = [];
+// const flours: Flour[] = [];
 
-const newBread: Bread = {
-  name: "Pão de banana",
-  ingredients: ['farinha de aveia sem glúten', 'bananas maduras', 'nozes', 'ovos', 'mel'],
-  gluten: false,
-};
+// const newBread: Bread = {
+//   name: "Pão de banana",
+//   ingredients: ['farinha de aveia sem glúten', 'bananas maduras', 'nozes', 'ovos', 'mel'],
+//   gluten: false,
+// };
 
-const newFlour: Flour = {
-  brand: "Dona Benta",
-  description: "Farinha de trigo enriquecida com ferro e ácido fólico.",
-  gluten: true,
-};
+// const newFlour: Flour = {
+//   brand: "Dona Benta",
+//   description: "Farinha de trigo enriquecida com ferro e ácido fólico.",
+//   gluten: true,
+// };
 
-const bread = addProducts(breads, newBread);
-const flour = addProducts(flours, newFlour);
+// const bread = addProducts(breads, newBread);
+// const flour = addProducts(flours, newFlour);
 
-console.log('bread', bread);
-console.log('flour', flour);
+// console.log('bread', bread);
+// console.log('flour', flour);
